a68d8a78964b19a472752d7e499000a13625539e
AlexPanbc <panbingcan@sina.com>
2018-02-04 11:28:27 +0800

    加入定时器

--- src/main/java/cn/pbc/activemq/controller/ProducerController.java
+++ src/main/java/cn/pbc/activemq/controller/ProducerController.java
@@ -2,17 +2,21 @@ package cn.pbc.activemq.controller;
 
 import cn.pbc.activemq.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Alex on 2018/2/4.
  */
+@EnableScheduling
 @RestController
 public class ProducerController {
     @Autowired
     private Producer producer;
 
+    @Scheduled(fixedDelay=3000)//每3s执行1次
     @GetMapping(value = "/sendqueue")
     public void sendQueue() {
         for (int i = 0; i < 100; i++)
